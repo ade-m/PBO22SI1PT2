04f77a75a98c9dacaec3f76d1e6b08d07771a190
farrelraihan <113604965+farrelraihan@users.noreply.github.com>
2023-05-24 10:42:20 +0700

    Update App.java
    Farrel meninggalkan jejak

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Dunia!");
+        
+        System.out.println("Farrel was here");
     }
 }
