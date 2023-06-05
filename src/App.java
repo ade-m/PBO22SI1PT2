a63c574b18d031b5443061afa982c5730e3b8e63
ade-m <66313509+ade-m@users.noreply.github.com>
2023-06-05 10:19:31 +0700

    konstruktor dan init

--- src/App.java
+++ src/App.java
@@ -1,8 +1,23 @@
+import model.Mahasiswa;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Dunia!");
-        
-        System.out.println("Farrel was here");
-        System.out.println("PUAN PRESIDEN!!!");
+        String namaMahasiswa = "Farrel";
+        String namaKuliah ="Matematika Diskrit";
+        int semester =1;
+        String nilaiMatakuliah = "D";
+
+        Mahasiswa farrel=new Mahasiswa();
+        System.out.println(farrel);
+        farrel = new Mahasiswa("Farrel", "Jl. Medan");
+        System.out.println(farrel);
+        Mahasiswa jerry = new Mahasiswa("Jerry","030081211",
+                    "Jl.jl Medan","Sistem Informasi");
+        System.out.println(jerry);
     }
+    public static void cetakMahasiswa(Mahasiswa x){
+        System.out.println(x.getNama() + " "+ x.getAlamat());
+    }
+    public void hitungIPK(){}
+    public void retake(){}
 }
--- src/model/Mahasiswa.java
+++ src/model/Mahasiswa.java
@@ -5,6 +5,67 @@ package model;
  */
 public class Mahasiswa {
     /*
-     * deskripsi
+     * deskripsi nama, studentid, alamat, program studi
      */
+    String nama;
+    String studentId;
+    String alamat;
+    String programStudi;
+    public Mahasiswa(){
+    }
+
+    public Mahasiswa(String nama, String studentId, String alamat, String programStudi) {
+        this.nama = nama;
+        this.studentId = studentId;
+        this.alamat = alamat;
+        this.programStudi = programStudi;
+    }
+    
+    public Mahasiswa(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getStudentId() {
+        return this.studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getAlamat() {
+        return this.alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getProgramStudi() {
+        return this.programStudi;
+    }
+
+    public void setProgramStudi(String programStudi) {
+        this.programStudi = programStudi;
+    }
+
+    @Override
+    public String toString() {
+        return "|" +
+            " " + getNama() + "\t|" +
+            " " + getStudentId() + "\t|" +
+            " " + getAlamat() + "\t|" +
+            " " + getProgramStudi() + "\t|" +
+            "";
+    }
+
 }
