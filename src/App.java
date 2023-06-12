a6dd60b808d7ccd27a2c0fe9a6363da96ee167d9
ade-m <66313509+ade-m@users.noreply.github.com>
2023-06-12 11:27:32 +0700

    input data

--- src/App.java
+++ src/App.java
@@ -1,20 +1,16 @@
 import model.Mahasiswa;
 
 public class App {
+    static Mahasiswa mahasiswa[]= new Mahasiswa[20];
     public static void main(String[] args) throws Exception {
        init();
-        String namaMahasiswa = "Farrel";
-        String namaKuliah ="Matematika Diskrit";
-        int semester =1;
-        String nilaiMatakuliah = "D";
-
-        Mahasiswa farrel=new Mahasiswa();
-        System.out.println(farrel);
-        farrel = new Mahasiswa("Farrel", "Jl. Medan");
-        System.out.println(farrel);
-        Mahasiswa jerry = new Mahasiswa("Jerry","030081211",
-                    "Jl.jl Medan","Sistem Informasi");
-        System.out.println(jerry);
+        cetakDataMahasiswa();
+    }
+    public static void cetakDataMahasiswa(){
+        for(int i =0;i<20;i++){
+            if(mahasiswa[i]!=null)
+                System.out.println(mahasiswa[i]);
+        }
     }
     public static void init(){
         Mahasiswa owen = new Mahasiswa("Owen","030081211",
@@ -28,6 +24,11 @@ public class App {
 
         Mahasiswa Tyne = new Mahasiswa("Tyne Theodora","030081211",
                     "Jl.jl Medan","Sistem Informasi");
+        mahasiswa[0]=jerry;
+        mahasiswa[1]= Tyne;
+        mahasiswa[2]=vanessa;
+        mahasiswa[3]=salim;
+        mahasiswa[4]= owen;
     }
     public static void cetakMahasiswa(Mahasiswa x){
         System.out.println(x.getNama() + " "+ x.getAlamat());
