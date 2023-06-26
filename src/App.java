33ce73bb85d5257526b90eea4e469fdfd3ff4590
ade-m <66313509+ade-m@users.noreply.github.com>
2023-06-26 10:19:51 +0700

    arraylist

--- src/App.java
+++ src/App.java
@@ -1,34 +1,36 @@
+import java.util.ArrayList;
+
 import model.Mahasiswa;
 
 public class App {
-    static Mahasiswa mahasiswa[]= new Mahasiswa[20];
+    static ArrayList<Mahasiswa> mahasiswa= new ArrayList<Mahasiswa>();
     public static void main(String[] args) throws Exception {
        init();
         cetakDataMahasiswa();
     }
     public static void cetakDataMahasiswa(){
-        for(int i =0;i<20;i++){
-            if(mahasiswa[i]!=null)
-                System.out.println(mahasiswa[i]);
+        for(int i =0;i<mahasiswa.size();i++){
+            if(mahasiswa.get(i)!=null)
+                System.out.println(mahasiswa.get(i));
+        }
+        for (Mahasiswa mahasiswa2 : mahasiswa) {
+            if(mahasiswa2!=null)
+                System.out.println(mahasiswa2);            
         }
     }
     public static void init(){
-        Mahasiswa owen = new Mahasiswa("Owen","030081211",
-                    "Jl.jl Medan","Sistem Informasi");
-        Mahasiswa jerry = new Mahasiswa("Jerry","030081211",
-                    "Jl.jl Medan","Sistem Informasi");
-        Mahasiswa vanessa = new Mahasiswa("Vannessa","030081211",
-                    "Jl.jl Medan","Sistem Informasi");
-        Mahasiswa salim = new Mahasiswa("Salim","030081211",
-                    "Jl.jl Medan","Sistem Informasi");
-
-        Mahasiswa Tyne = new Mahasiswa("Tyne Theodora","030081211",
-                    "Jl.jl Medan","Sistem Informasi");
-        mahasiswa[0]=jerry;
-        mahasiswa[1]= Tyne;
-        mahasiswa[2]=vanessa;
-        mahasiswa[3]=salim;
-        mahasiswa[4]= owen;
+        Mahasiswa[] mhs = new Mahasiswa[20];
+        mahasiswa.add(new Mahasiswa("Owen","030081211",
+                    "Jl.jl Medan","Sistem Informasi"));
+         mahasiswa.add(new Mahasiswa("Jerry","030081211",
+                    "Jl.jl Medan","Sistem Informasi"));
+         mahasiswa.add(new Mahasiswa("Vannessa","030081211",
+                    "Jl.jl Medan","Sistem Informasi"));
+         mahasiswa.add(new Mahasiswa("Salim","030081211",
+                    "Jl.jl Medan","Sistem Informasi"));
+         mahasiswa.add(new Mahasiswa("Tyne Theodora","030081211",
+                    "Jl.jl Medan","Sistem Informasi"));
+       // mahasiswa.add(mhs[0]);
     }
     public static void cetakMahasiswa(Mahasiswa x){
         System.out.println(x.getNama() + " "+ x.getAlamat());
