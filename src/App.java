bfa81cb1407a96508d5af272a755a9af2a42f631
ade-m <66313509+ade-m@users.noreply.github.com>
2023-06-26 10:30:28 +0700

    Input dan Delete

--- src/App.java
+++ src/App.java
@@ -1,18 +1,38 @@
 import java.util.ArrayList;
-
+import java.util.Scanner;
 import model.Mahasiswa;
 
 public class App {
     static ArrayList<Mahasiswa> mahasiswa= new ArrayList<Mahasiswa>();
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
        init();
+
+        System.out.println("setelah init");
+        cetakDataMahasiswa();
+        inputDataMahasiswa();
+        System.out.println("setelah input data");
         cetakDataMahasiswa();
     }
+    public static void inputDataMahasiswa(){
+        String nama, programStudi,alamat,studentID;
+        System.out.print("Nama \t:");
+        nama = input.nextLine();
+        System.out.print("Student ID \t:");
+        studentID=   input.nextLine();
+        System.out.print("Program Studi \t:");
+        programStudi = input.nextLine();
+        System.out.print("Alamat \t:");
+        alamat  = input.nextLine();
+        mahasiswa.add(new Mahasiswa(nama,studentID, alamat,programStudi));
+        mahasiswa.remove(4);
+    }
+
     public static void cetakDataMahasiswa(){
-        for(int i =0;i<mahasiswa.size();i++){
-            if(mahasiswa.get(i)!=null)
-                System.out.println(mahasiswa.get(i));
-        }
+        // for(int i =0;i<mahasiswa.size();i++){
+        //     if(mahasiswa.get(i)!=null)
+        //         System.out.println(mahasiswa.get(i));
+        // }
         for (Mahasiswa mahasiswa2 : mahasiswa) {
             if(mahasiswa2!=null)
                 System.out.println(mahasiswa2);            
